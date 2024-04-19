39d4a84df638d8eccee38985ac906a8fc730f5b4
Chandan Singh <chandan_singh@berkeley.edu>
2024-04-19 14:13:40 -0700

    clean up logging

--- augdistill/notebooks/word_distr.py
+++ augdistill/notebooks/word_distr.py
@@ -2,16 +2,11 @@ import warnings
 import re
 from tqdm import tqdm
 import torch
-from os.path import join
-import transformers
 import pandas as pd
 import transformers
 import scipy
-import os
-import datasets
-import sys
-from copy import deepcopy
 import numpy as np
+import logging
 
 
 def get_next_word_distr(
@@ -21,6 +16,7 @@ def get_next_word_distr(
     num_tokens: int = 2,
     cdf_threshold: float = 0.8,
     batch_size: int = 2048,
+    use_complete_calc_final_token: bool = False,
 ) -> pd.DataFrame:
     '''Given an LM, get the distribution of the next word. Since LMs use tokens, this can vary from the next-token distr.
     To solve this issue, we compute the prob distr. for 2 or 3 tokens, then merge the probabilities that yield the same leading word.
@@ -38,8 +34,12 @@ def get_next_word_distr(
         2 or 3. How many tokens do we use to compute the distr?
     cdf_threshold (0-1)
         Instead of computing the entire distribution, we can compute the distribution of the top cdf_threshold fraction of the probability mass at each token
-        If we sample 2 tokens, we will then have the top cdf_threshold probability mass
-        For 3 tokens, we will have the top cdf_threshold^2 probability mass
+        If we sample 2 tokens, we will then have the top cdf_threshold^2 probability mass
+        For 3 tokens, we will have the top cdf_threshold^3 probability mass
+    use_complete_calc_final_token
+        If True, will compute the entire distr for the final token,
+        so for 2 tokens will move from cdf_threshold^2->cdf_threshold^1 and for 3 tokens from cdf_threshold^3 -> cdf_threshold^2
+        However, this increases the computation time considerably
 
     Returns
     -------
@@ -63,7 +63,7 @@ def get_next_word_distr(
     # ngram_probs.append(tok0_probs[tok0_argsort[:num_selected_tok0]])
     tok0_idxs = tok0_argsort[:num_selected_tok0]
     tok0_ids = tok0_idxs  # token ids
-    print('num_tokens_selected', num_selected_tok0)
+    logging.debug(f'num_tokens_selected={num_selected_tok0}')
     if num_tokens == 1:
         generations = [tokenizer.decode([tok_id]) for tok_id in tok0_ids]
         return _convert_to_word_distr(generations, tok0_probs[tok0_idxs])
@@ -85,7 +85,7 @@ def get_next_word_distr(
     tok1_logits = tok1_logits.numpy().astype(np.double)
 
     # this has prob for each token given the prefix
-    print('softmax...')
+    logging.debug('softmax...')
     tok1_probs_conditional = scipy.special.softmax(tok1_logits, axis=1)
 
     # multiply by prob of tok0 to get raw probs
@@ -95,7 +95,7 @@ def get_next_word_distr(
                        tok0_probs[tok0_idxs], atol=1e-6)
     assert np.allclose(tok1_probs.sum(), cdf_threshold, atol=1e-1)
 
-    if num_tokens > 2:
+    if num_tokens > 2 or not use_complete_calc_final_token:
         # get top token idxs
         tok1_argsort = np.argsort(tok1_probs.flatten())[::-1]
         tok1_cdf = np.cumsum(tok1_probs.flatten()[tok1_argsort])
@@ -115,20 +115,18 @@ def get_next_word_distr(
         probs_ids_1 = tok1_probs.flatten()
 
     # convert to row, col idxs and recreate tokens
-    print('unraveling...')
-
     if num_tokens == 2:
-        tokens_list = [[tok0_ids_1[i], tok1_ids_1[i]]
+        # tokenizer decoding is actually the bottleneck
+        tokens_list = [(tok0_ids_1[i], tok1_ids_1[i])
                        for i in range(len(tok0_ids_1))]
-        # generations = tokenizer.batch_decode(tokens_list)
-        # decode in batches
-        batch_size = 4096
-        generations = []
-        for i in tqdm(range(0, len(tokens_list), batch_size)):
-            generations[i: i + batch_size] = tokenizer.batch_decode(
-                tokens_list[i: i + batch_size])
+        # generations = []
+        # tokenizer_batch_size = 4096
+        # for i in tqdm(range(0, len(tokens_list), tokenizer_batch_size)):
+        # generations[i: i + batch_size] = tokenizer.batch_decode(
+        # tokens_list[i: i + batch_size])
+        generations = tokenizer.batch_decode(tokens_list)
 
-        print('convert to word distr...')
+        logging.debug('convert to word distr...')
         return _convert_to_word_distr(generations, probs_ids_1)
 
     # 3 tokens ##########################
@@ -138,7 +136,7 @@ def get_next_word_distr(
             (prefix_embs, torch.LongTensor([tok0_ids_1[i]]), torch.LongTensor([tok1_ids_1[i]])))
         for i in range(num_selected_tok1)
     ])
-    print('num_tokens2', num_selected_tok1)
+    logging.debug(f'num_tokens2={num_selected_tok1}')
     tok2_logits = torch.zeros((num_selected_tok1, voc_size))
     for i in tqdm(range(0, num_selected_tok1, batch_size)):
         input_ids = input_ids_tok2[i:i + batch_size]
@@ -149,14 +147,15 @@ def get_next_word_distr(
     tok2_probs = tok2_probs_conditional * probs_ids_1.reshape(-1, 1)
 
     # get top token idxs
-    if num_tokens > 3:
+    if num_tokens > 3 or not use_complete_calc_final_token:
         tok2_argsort = np.argsort(tok2_probs.flatten())[::-1]
         tok2_cdf = np.cumsum(tok2_probs.flatten()[tok2_argsort])
         num_selected_tok2 = np.argmax(
             tok2_cdf > cdf_threshold * cdf_threshold * cdf_threshold)
+        logging.debug(f'num_tokens2 processing={num_selected_tok2}')
         tok2_selected_idxs = tok2_argsort[:num_selected_tok2]
     else:
-        tok2_selected_idxs = np.arange(tok2_probs.shape[1])
+        tok2_selected_idxs = np.arange(tok2_probs.size)
 
     # convert to row, col idxs and recreate tokens
     tok2_idxs_selected_rows, tok2_idxs_selected_cols = np.unravel_index(
@@ -166,8 +165,9 @@ def get_next_word_distr(
     tok2_ids_2 = tok2_idxs_selected_cols
     probs_ids_2 = tok2_probs[tok2_idxs_selected_rows, tok2_idxs_selected_cols]
 
-    generations = [tokenizer.decode([tok0_ids_2[i], tok1_ids_2[i], tok2_ids_2[i]])
+    tokens_list = [(tok0_ids_2[i], tok1_ids_2[i], tok2_ids_2[i])
                    for i in range(len(tok0_ids_2))]
+    generations = tokenizer.batch_decode(tokens_list)
     return _convert_to_word_distr(generations, probs_ids_2)
 
 
@@ -177,16 +177,18 @@ def _convert_to_word_distr(generations, probs) -> pd.DataFrame:
         'prob': probs
     })
 
-    def remove_punctuation(s):
-        return re.sub(r'[^\w\s]', '', s)
-
     def is_whitespace(s):
         return re.match(r'^\s*$', s) is not None
 
+    def is_all_letters(input_string):
+        # This regex matches a string that only contains alphabetic characters and is at least one character long
+        return bool(re.match(r'^[a-zA-Z]+$', input_string))
+
     def get_first_word(s):
-        # strip lowercase and remove punctuation
+        # strip lowercase
         s = s.strip().lower()
-        s = remove_punctuation(s)
+        # remove punctuation
+        s = re.sub(r'[^\w\s]', '', s)
         words = s.split()
         if len(words) == 0:
             return ' '
@@ -194,12 +196,19 @@ def _convert_to_word_distr(generations, probs) -> pd.DataFrame:
 
     df['generation'] = df['generation'].apply(get_first_word)
 
-    # remove any rows that are just whitespace
+    # remove any rows that contain just whitespace
     idxs_whitespace = df['generation'].apply(is_whitespace)
     if idxs_whitespace.sum() > 0:
         warnings.warn(
             f'removing {idxs_whitespace.sum()} whitespace rows accounting for {df[idxs_whitespace]["prob"].sum()} prob')
     df = df[~df['generation'].apply(is_whitespace)]
+
+    # remove any rows that contain non letters
+    idxs_nonletters = ~df['generation'].apply(is_all_letters)
+    if idxs_nonletters.sum() > 0:
+        logging.debug(
+            f'removing {idxs_nonletters.sum()} non-letter elements accounting for {df[idxs_nonletters]["prob"].sum()} prob')
+    df = df[~idxs_nonletters]
 
     # merge prob based on first_word
     df = df.groupby('generation')['prob'].sum(
@@ -210,18 +219,24 @@ def _convert_to_word_distr(generations, probs) -> pd.DataFrame:
 if __name__ == '__main__':
     import imodelsx.llm
 
+    # set logging to debug
+    logging.basicConfig(level=logging.DEBUG)
+
     # these are just normal HF models/tokenizers
-    checkpoint = 'gpt2-xl'
+    # checkpoint = 'gpt2-xl'
+    checkpoint = 'meta-llama/Meta-Llama-3-8B'
     model = imodelsx.llm.load_hf_model(checkpoint)
     tokenizer = imodelsx.llm.load_tokenizer(checkpoint)
+    logging.debug('voc_size ' + str(len(tokenizer)))
 
     d = get_next_word_distr(
         prefix='My favorite ice cream flavor is',
         model=model,
         tokenizer=tokenizer,
         num_tokens=2,
-        cdf_threshold=0.7,
-        batch_size=2048,
+        cdf_threshold=0.8,
+        # batch_size=512,
+        batch_size=64,
     )
 
-    display(d)
+    print(d.head(100))
